71e1a5ea24a4a8b0a294ece1a546396baf493fe6
Kshitiz Shakya <kshitiz.shakya22@gmail.com>
2025-08-30 21:17:24 +0545

    Made some changes wip

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,11 +2,11 @@ local function augroup(name)
   return vim.api.nvim_create_augroup('lazyvim_' .. name, { clear = true })
 end
 
-vim.api.nvim_create_autocmd('VimEnter', {
-  group = augroup 'autoupdate',
-  callback = function()
-    require('lazy').update {
-      show = false,
-    }
-  end,
-})
+
+vim.api.nvim_create_user_command('RefreshAwsCreds', function(opts)
+  os.execute('~/aws_sts.sh ' .. opts.args)
+end, { nargs = 1 })
+
+vim.api.nvim_create_user_command('RefreshAwsCredsSpark', function(opts)
+  os.execute('~/aws_assume_role_mfa.sh ' .. opts.args)
+end, { nargs = 1 })
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,20 +1,9 @@
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  { import = 'KshitizGIT.plugins' },
-}, {
+  spec = { import = 'plugins' }  ,
+  checker = { enabled = true },
+},
+{
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
@@ -32,5 +21,3 @@ require('lazy').setup({
     },
   },
 })
-
--- vim: ts=2 sts=2 sw=2 et
